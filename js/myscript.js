290a9d6ca670c4362adce611a8793966ee8399c1
JakeG-2710 <jakeg2710@gmail.com>
2023-11-10 10:30:12 +0000

    googke

--- js/myscript.js
+++ js/myscript.js
@@ -2,7 +2,8 @@
 (function(){
 
     document.getElementById("myHeading").innerHTML="Jake";
-    document.querySelector("nav ul li").setAttribute("class", "currentPage");
+    //document.querySelector("nav ul li").setAttribute("class", "currentPage");
+    document.querySelector("nav ul li").setAttribute("href", "https://www.google.co.uk");
 
 })();
 
