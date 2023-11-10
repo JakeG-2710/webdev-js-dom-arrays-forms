3e7575995a672db19862dd06f1629317f591d978
JakeG-2710 <jakeg2710@gmail.com>
2023-11-10 10:40:50 +0000

    js

--- js/myscript.js
+++ js/myscript.js
@@ -2,8 +2,11 @@
 (function(){
 
     document.getElementById("myHeading").innerHTML="Jake";
-    //document.querySelector("nav ul li").setAttribute("class", "currentPage");
-    document.querySelector("nav ul li").setAttribute("href", "https://www.google.co.uk");
+    document.querySelector("nav ul li").setAttribute("class", "currentPage");
+
+    document.getElementById("myTestEvent").onclick=function(){
+        console.info("HELLO")
+    }
 
 })();
 
