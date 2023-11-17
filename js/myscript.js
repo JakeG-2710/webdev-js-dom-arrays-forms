2953bd2ed16d8ed1bb87587d90de8951e16de133
JakeG-2710 <jakeg2710@gmail.com>
2023-11-17 09:29:40 +0000

    asas

--- js/myscript.js
+++ js/myscript.js
@@ -21,6 +21,9 @@
     document.querySelector(".reset").addEventListener("click",function(ev){ 
         document.querySelector("body").removeAttribute("class")
     })
+
+    let imageAr=['images/views1.jpg','images/views2.jpg','images/views3.jpg','images/views4.jpg','images/views5.jpg','images/views6.jpg'];
+    document.getElementById('myImages').setAttribute('scr',imageAr[2]);
 })();
 
   
